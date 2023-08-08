f50c1957cb1071ed234850cb68e586c421515f59
peter <apppeterpan@gmail.com>
2023-08-08 12:26:41 +0800

    Initial Commit

---
+++ DrinkApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DrinkApp
+//
+//  Created by Peter Pan on 2023/8/8.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DrinkApp/DrinkAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrinkAppApp.swift
+//  DrinkApp
+//
+//  Created by Peter Pan on 2023/8/8.
+//
+
+import SwiftUI
+
+@main
+struct DrinkAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
